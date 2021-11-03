f70690bfb3112deee20e165da463e2f4d1baee29
Pal-Mel <54739062+Pal-Mel@users.noreply.github.com>
2021-11-03 21:02:32 +0200

    master

--- user/models.py
+++ user/models.py
@@ -10,6 +10,8 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+    phone = models.CharField(max_length=13,blank=True)
+    wokrplace = models.CharField(max_length=50)
 
     def __str__(self):
         return 'Профіль користувача {}'.format(self.user.username)
