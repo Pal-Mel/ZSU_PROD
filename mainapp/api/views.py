40e23b5831a6469b62930e381f55efc2e08bf3e6
Pal-Mel <54739062+Pal-Mel@users.noreply.github.com>
2021-11-11 13:15:15 +0200

    main

--- mainapp/api/views.py
+++ mainapp/api/views.py
@@ -3,5 +3,5 @@ from rest_framework.response import Response
 
 class TestApiView(APIView):
     def get(self, request, *args, **kwargs):
-        data = [{'id':1, 'name':'djon'},{'id':2, 'name':'fill'} ]
+        data = [{'id':1, 'name':'Джон'},{'id':2, 'name':'fill'} ]
         return Response(data)
